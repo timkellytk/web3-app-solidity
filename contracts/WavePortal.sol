b6de7ed97dd982bba7da54eb8c9e1ed6e5064995
Tim Kelly <timpkelly1@gmail.com>
2021-08-14 09:21:21 +1000

    Adding basic files, typescript, airbnb, eslint and prettier config rules

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// Use X compiler
+pragma solidity ^0.8.0;
+
+// Dependencies
+import "hardhat/console.sol";
+
+// Contract
+contract WavePortal {
+	constructor() {
+		console.log("WavePortal created");
+	}	
+}
