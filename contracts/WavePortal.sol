d2cef5b190f71d9d734a32f50aa2f8d40cf4a9a6
Tim Kelly <timpkelly1@gmail.com>
2021-08-14 11:40:34 +1000

    First seminar complete

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,9 +4,30 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// Contract
 contract WavePortal {
+	uint totalWaves;
+	address biggestWaveAddy;
 	constructor() {
 		console.log("WavePortal created");
 	}	
+
+	function wave() public {
+		totalWaves += 1;
+		console.log("%s is waved!", msg.sender);
+	}
+
+	function scoreBiggestWave() public {
+		biggestWaveAddy = msg.sender;
+		console.log("%s scored the biggest wave!", biggestWaveAddy);
+	}
+
+	function getTotalWaves() public view returns (uint) {
+		console.log("We have %d total waves", totalWaves);
+		return totalWaves;
+	}
+
+	function getBiggestWave() public view returns (address) {
+		console.log("We have %s as the biggest wave recorded", biggestWaveAddy);
+		return biggestWaveAddy;
+	}
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -1,10 +1,33 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owner, randoPerson] = await ethers.getSigners();
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
+
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  const randoWaveTxn = await waveContract.connect(randoPerson).wave();
+  await randoWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
+
+  // Custom solidity code
+  const randoBiggestWave = await waveContract
+    .connect(randoPerson)
+    .scoreBiggestWave();
+  await randoBiggestWave.wait();
+
+  await waveContract.getBiggestWave();
 }
 
 main()
