b89ca0f8493470a3a2bb35849affc4923fc2e931
Tim Kelly <timpkelly1@gmail.com>
2021-08-25 18:44:24 +1000

    Updated deploy.ts for better readability

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the acount: ", deployer.address);
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
+
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy({
     value: ethers.utils.parseEther("0.1"),
